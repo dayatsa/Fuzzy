c57cd7cc45fe05cd4fc17546071368300769e526
Hidayat Eko Saputro <hidayat.e.s@mail.ugm.ac.id>
2019-11-15 16:38:16 +0700

    edit library

--- fuzzy_coba.cpp
+++ fuzzy_coba.cpp
@@ -2,3 +2,13 @@
 #include "fuzzy_coba.h"
 #include <math.h>
 
+Fuzzy::Fuzzy(int nb_, int nm_, int ns_, int zero_, int ps_, int pm_, int pb_, int selisih_) {
+    nb = nb_;
+    nm = nm_;
+    ns = ns_;
+    zero = zero_;
+    ps = ps_;
+    pm = pm_;
+    pb = pb_;
+    selisih = selisih_;
+}
--- fuzzy_coba.h
+++ fuzzy_coba.h
@@ -3,24 +3,12 @@
 
 class Fuzzy
 {
+    public:
+    Fuzzy(int nb_, int nm_, int ns_, int zero_, int ps_, int pm_, int pb_, int selisih_);
 
-public:
-  Fuzzy(float mea_e, float est_e, float q);
-  float updateEstimate(float mea);
-  void setMeasurementError(float mea_e);
-  void setEstimateError(float est_e);
-  void setProcessNoise(float q);
-  float getKalmanGain();
-  float getEstimateError();
 
-private:
-  float _err_measure;
-  float _err_estimate;
-  float _q;
-  float _current_estimate;
-  float _last_estimate;
-  float _kalman_gain;
-
+    private:
+    int nb, nm, ns, zero, ps, pm, pb, selisih;
 };
 
 #endif
